c0fcaf306e169a51f092416fbd028ad5739b68fb
ridwanyah24 <ridwangames24@gmail.com>
2025-07-26 04:54:39 +0100

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/App.css 	modified:   src/components/Providers.tsx 	modified:   tsconfig.json

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import Navigation from "./Navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
